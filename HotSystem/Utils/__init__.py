1b28d73cfd427e1f00e8fbdf88123efa52e0d3d4
shai <shai@quantumtransistors.com>
2024-12-26 12:27:54 +0200

    Added support for multiple SRS SIM960 in the config gui + fixed simulation handling

--- HotSystem/Utils/__init__.py
+++ HotSystem/Utils/__init__.py
@@ -2,7 +2,7 @@ from .serial_device import SerialDevice
 from .Common import (load_scan_plane_calibration_data, save_scan_plane_calibration_data, calculate_plane_normal,
                      generate_scan_points, generate_z_series, scatter_scan_points,
                      intensity_to_rgb_heatmap_normalized, calculate_z_series, open_file_dialog,
-                     get_available_xml_files, get_square_matrix_size, remove_overlap_from_string, select_csv_file, loadFromCSV, open_dialog)
+                     get_available_xml_files, get_square_matrix_size, remove_overlap_from_string, select_csv_file, loadFromCSV, open_dialog, scan_com_ports)
 from .intensity_peak_finding import OptimizerMethod, find_max_signal
 from .scan_utils import create_scan_vectors
 
@@ -25,5 +25,6 @@ __all__ = [
     "select_csv_file",
     "loadFromCSV"
     "find_max_signal",
-    "create_scan_vectors"
+    "create_scan_vectors",
+    "scan_com_ports"
 ]
