14769a363cbd1652bc78f524273b5666d507c6be
qt-boaz <boaz@quantumtransistors.com>
2025-01-01 17:00:03 +0200

    improved theme loading

--- HotSystem/SystemConfig/__init__.py
+++ HotSystem/SystemConfig/__init__.py
@@ -1,6 +1,6 @@
 from .system_config import (SystemConfig, SystemType, Instruments, load_system_from_xml, load_system_config, Device,
                             find_ethernet_device, InstrumentsAddress)
-from .system_config_gui import save_to_xml, run_system_config_gui, load_instrument_images
+from .system_config_gui import save_to_xml, run_system_config_gui, load_instrument_images, create_themes
 
 from .QuaConfigs import QUAConfigBase, HotSystemQuaConfig, FemtoQuaConfig, AttoQuaConfig, QuaConfigSelector
 
@@ -19,5 +19,6 @@ __all__ = ["SystemConfig",
            "FemtoQuaConfig",
            "QuaConfigSelector",
            "InstrumentsAddress",
-           "AttoQuaConfig"
+           "AttoQuaConfig",
+           "create_themes"
            ]
