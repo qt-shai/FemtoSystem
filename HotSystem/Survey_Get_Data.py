9a2c7987caee5537f4e37a91ad7b8eef81d40985
qt-Daniel <daniel@quantumtransistors.com>
2025-03-02 17:48:37 +0200

    Added functionality of returning position upon mouse click, but the numbers are wrong

--- HotSystem/Survey_Get_Data.py
+++ HotSystem/Survey_Get_Data.py
@@ -4,12 +4,14 @@ from Utils.Common import loadFromCSV
 from Utils import open_file_dialog
 import matplotlib.pyplot as plt
 import os
+from scipy import stats
 
 class GUI_Survey_Interface:
     def __init__(self):
+        self.total_selected_points = []
         self.endLoc = None
         self.startLoc = None
-        self.file_path = "C://users//daniel//Work Folders//Documents//2024_7_1_7_5_41scan.csv"
+        self.file_path = "C://users//daniel//Work Folders//Documents//"
         self.win_pos = None
         self.win_w = 1200
         self.win_h = 800
@@ -23,6 +25,9 @@ class GUI_Survey_Interface:
         self.result_arrayXY_ = None
         self.arrXY = None
         self.idx_scan = None
+        self.selection_mode = True
+        self.selected_points = []
+        self.image_id = None
 
     def GetWindowSize(self) -> None:
         win_size = [self.win_w, self.win_h]
@@ -46,30 +51,24 @@ class GUI_Survey_Interface:
         print("Loaded csv data from file")
 
     def intensity_to_rgb_heatmap(self, intensity):
-        # Define a colormap (you can choose any colormap from Matplotlib)
-        # cmap = plt.get_cmap('hot')
         cmap = plt.get_cmap('jet')
-
-        # Normalize the intensity to the range [0, 1] (if necessary)
         intensity = max(0, min(0.99999999, intensity))
-
-        # Map the intensity value to a color in the colormap
         rgba_color = cmap(intensity)
-
-        # Convert RGBA tuple to RGB tuple (discard alpha channel)
         rgb_color = tuple(int(rgba_color[i] * 255) for i in range(4))
 
         return rgb_color
 
     def process_data(self):
         self.data = np.array(self.data)
-        self.scan_data = self.data[0:, 3].astype(float)
+        self.intensity_data = self.data[0:, 3].astype(float)
         self.im_arr_x = self.data[0:, 4].astype(float) / 1e6  # x data of the Smaract values from the csv
         self.im_arr_y = self.data[0:, 5].astype(float) / 1e6  # y data of the Smaract values from the csv
         self.im_arr_z = self.data[0:, 6].astype(float) / 1e6
 
         Nx = int(round((self.im_arr_x[-1] - self.im_arr_x[0]) / (self.im_arr_x[1] - self.im_arr_x[0])) + 1)
         Ny = int(round((self.im_arr_y[-1] - self.im_arr_y[0]) / (self.im_arr_y[Nx] - self.im_arr_y[0])) + 1)
+        # Nx = len(np.unique(self.im_arr_x))
+        # Ny = len(np.unique(self.im_arr_y))
 
         self.startLoc = [int(self.data[1, 4].astype(float) / 1e6), int(self.data[1, 5].astype(float) / 1e6),
                          int(self.data[1, 6].astype(float) / 1e6)]  # um
@@ -77,8 +76,12 @@ class GUI_Survey_Interface:
                        int(self.data[-1, 6].astype(float) / 1e6)]  # um
 
         #I feel like I only need one of those since z is constant
-        self.arrXY = np.flipud(np.reshape(self.scan_data, (Nx,Ny))) #Needs to be reshaped to size (N_x,N_y,0). If the shape does not match Nx*Ny might fail
-        #arrXY = np.flipud(self.scan_data[, :, :])
+        self.scan_data = np.reshape(self.intensity_data[0:Nx*Ny], (Ny,Nx))
+        self.im_arr_x = self.im_arr_x[0:Nx]
+        self.im_arr_y = self.im_arr_y[0:Nx * Ny:Nx]
+        print(self.scan_data.shape)
+        self.arrXY = np.flipud(self.scan_data) #Needs to be reshaped to size (N_x,N_y,0). If the shape does not match Nx*Ny might fail
+        #print(self.arrXY.shape)
 
         self.result_arrayXY = (self.arrXY * 255 / self.arrXY.max())
         self.result_arrayXY_ = []
@@ -90,12 +93,14 @@ class GUI_Survey_Interface:
 
 
     def queryXY_callback(self, app_data):
-        # print("queryXY_callback")
         a = dpg.get_plot_query_area(app_data)
         if np.any(a):
-            # Find the closest index in Yv for a[3]
+            # If it's a point click, x_min ~ x_max and y_min ~ y_max.
+            x_click = (a[0] + a[2]) / 2
+            y_click = (a[1] + a[3]) / 2
+            print(f"Clicked at: ({x_click}, {y_click})")
+        if np.any(a):
             y_index = np.argmin(np.abs(self.im_arr_y - a[3]))
-            # Find the closest index in Xv for a[1]
             x_index = np.argmin(np.abs(self.im_arr_x - a[1]))
 
             self.idx_scan[1] = y_index
@@ -113,6 +118,8 @@ class GUI_Survey_Interface:
             with dpg.group(horizontal=True):
                 dpg.add_button(label="Load Data", callback=self.get_data_from_csv)
                 dpg.add_button(label="Show Image", callback=self.add_image_graphics)
+                dpg.add_button(label="Start Selecting Points", callback=self.select_points, tag = "btnSelect")
+                dpg.add_button(label="Return all points", callback=self.return_all_clicks)
 
             dpg.add_texture_registry(show=False, tag="texture_reg")
 
@@ -136,7 +143,7 @@ class GUI_Survey_Interface:
                      crosshairs=True,
                      query=True, callback=self.queryXY_callback)
         dpg.add_plot_axis(dpg.mvXAxis, label="x axis, z=" + "{0:.2f}".format(self.im_arr_z[0]),
-                          parent="plotImage")
+                          parent="plotImage",tag="plotImage_X")
         dpg.add_plot_axis(dpg.mvYAxis, label="y axis", parent="plotImage", tag="plotImage_Y")
         dpg.add_image_series("textureXY_tag", bounds_min=[self.startLoc[0], self.startLoc[1]],
                              bounds_max=[self.endLoc[0], self.endLoc[1]],
@@ -150,11 +157,72 @@ class GUI_Survey_Interface:
         if (item_width is None) or (item_height is None):
             raise Exception("Window does not exist")
         dpg.set_item_height("Survey_Window", item_height + 150)
-        # with dpg.plot(label="title", height=-1, width=-1, crosshairs=True, anti_aliased=False, delay_search=True,
-        #               tag=f"_plot_with", parent = "Survey_Window"):
-        #     dpg.add_plot_axis(dpg.mvXAxis)
-        #     with dpg.plot_axis(dpg.mvYAxis):
-        #         dpg.add_image_series("textureXY_tag", [0, 0], [800, 800])
+
+    def configure_handler(self):
+        with dpg.item_handler_registry(tag="handler_registry"):
+            dpg.add_item_clicked_handler(callback=self.get_mouse_plot_coordinates)
+
+    def select_points(self):
+        if self.selection_mode:
+            dpg.configure_item("btnSelect", label="Stop Selecting Points")
+            self.selected_points = []
+            dpg.bind_item_handler_registry("plotImage", "handler_registry")
+        else:
+            dpg.configure_item("btnSelect", label="Start Selecting Points")
+            dpg.bind_item_handler_registry("plotImage", None)
+            # For now, simply print the selected points.
+            print("Recorded points:", self.selected_points)
+            # Clear stored points.
+            self.selected_points = []
+        self.selection_mode = not self.selection_mode
+
+    def get_mouse_plot_coordinates(self, sender, app_data):
+        """
+        Callback to convert the mouse click's screen position into plot data coordinates.
+        """
+        # Get the current global mouse position (in screen coordinates)
+        mouse_pos = dpg.get_mouse_pos()
+        print(f"mouse_pos: {mouse_pos}")
+        # Get the plot widget's screen position and size
+        plot_min = dpg.get_item_rect_min("plotImage")
+        plot_max = dpg.get_item_rect_max("plotImage")
+        print(f"plot_min: {plot_min}")
+        print(f"plot_max: {plot_max}")
+
+        if plot_min is None or plot_max is None:
+            print("Plot bounds not available.")
+            return
+
+        # Compute the size of the plot area in pixels
+        plot_width = plot_max[0] - plot_min[0]
+        plot_height = plot_max[1] - plot_min[1]
+        print(f"plot_width: {plot_width}")
+        print(f"plot_height: {plot_height}")
+
+        axis_x_lim = dpg.get_axis_limits("plotImage_X")
+        axis_y_lim = dpg.get_axis_limits("plotImage_Y")
+        print(f"axis_x_lim: {axis_x_lim}")
+        print(f"axis_y_lim: {axis_y_lim}")
+
+        # Determine the relative position of the mouse inside the plot area (0-1)
+        rel_x = mouse_pos[0]/ plot_width * ((axis_x_lim[1]) - (axis_x_lim[0])) #Update this according to whiteboard
+        rel_y = mouse_pos[1]/ plot_height * abs(axis_y_lim[1] - axis_y_lim[0]) #Update this according to whiteboard
+
+        print(f"self.startLoc: {self.startLoc}")
+        print(f"self.endLoc: {self.endLoc}")
+
+        # Use your plot's data bounds (startLoc and endLoc) to map the relative position to data coordinates.
+        # Assuming startLoc[0] is the left bound (min x) and endLoc[0] is the right bound (max x)
+        # and similarly for y.
+        data_x = axis_x_lim[0] + rel_x * (axis_x_lim[1] - axis_x_lim[0])
+        data_y = axis_y_lim[0] + (1 - rel_y) * (axis_y_lim[1] - axis_y_lim[0])
+
+        print(f"Mouse clicked at data coordinates: ({data_x}, {data_y})")
+        self.last_clicked = (data_x, data_y)
+        self.total_selected_points.append(self.last_clicked)
+
+    def return_all_clicks(self):
+        print(self.total_selected_points)
 
     def run_gui(self) -> None:
         dpg.create_viewport(title="Dear PyGui Survey Interface", width=self.viewport_width, height=self.viewport_height)
@@ -167,5 +235,6 @@ if __name__ == '__main__':
     dpg.create_context()
     gui = GUI_Survey_Interface()
     gui.GetWindowSize()
+    gui.configure_handler()
     gui.set_main_interface()
     gui.run_gui()
