5f1fd161af489f306261103eb665be49eedefa85
qt-Daniel <daniel@quantumtransistors.com>
2025-04-01 09:31:19 +0300

    Added arduino gui files

--- HotSystem/HW_GUI/GUI_arduino.py
+++ HotSystem/HW_GUI/GUI_arduino.py
@@ -304,6 +304,3 @@ class GUIArduino:
         # You can get the updated position from app_data or by using dpg.get_item_pos(sender)
         new_pos = dpg.get_item_pos(sender)
         # print(f"Window '{sender}' moved. New position: {new_pos}")
-
-
-
